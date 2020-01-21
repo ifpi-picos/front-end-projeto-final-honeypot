6edfd91c7a59814506098e30e4044d431c28ac43
Cristiana <you@example.com>
2020-01-21 20:18:12 -0300

    integração

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-function redirectIndex() {
-    window.location.assign("./index.html");
-}
-function redirectCadastroApicultores() {
-    window.location.assign('.pages/cadastroApicultores.html')
+const btn_salvar = document.querySelector('.cadastrar');
+
+// pegando os valores do formulario de cadastro de funcionarios
+function setFormularioFuncionario() {
+    const funcionario = {};
+    funcionario.nome = document.querySelector('.nome_func').value;
+    funcionario.username = document.querySelector('.username').value;
+    funcionario.funcao = document.querySelector('.funcao').value;
+    funcionario.email = document.querySelector('.email').value;
+    funcionario.senha = document.querySelector('.pass_func').value;
+
+    const url = 'http://localhost:3000/funcionario'
+
+    const option = {
+        method: 'POST',
+        body: JSON.stringify(funcionario),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch(url,option)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        }) 
+    console.log('dada');
 }
 
+function enviarDados(funcionario) {
+    
+}
+btn_salvar.addEventListener('click', () => {
+    setFormularioFuncionario();
+    
+})
